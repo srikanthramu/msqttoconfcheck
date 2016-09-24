e3f8211906903162c22ea890754d8b61575e535b
srikanthramu <srikanthramu@outlook.com>
2016-09-23 22:50:25 -0700

    corrected a message

--- src/main/java/net/mqtt/msqttoconfcheck/Messages.java
+++ src/main/java/net/mqtt/msqttoconfcheck/Messages.java
@@ -7,7 +7,7 @@ public class Messages {
 
 	public final static String BANNER = "------------------------------------\nReport\n------------------------------------\n";
 	public final static String END = "----------------------------------------------------------------------------------";
-	public final static String ALLOW_ANONYMOUS = "WARN: allow_anonymous is true - Allows clients with username and password to connect";
+	public final static String ALLOW_ANONYMOUS = "WARN: allow_anonymous is true - Allows clients without username and password to connect";
 	public final static String REQUIRE_CERTIFICATE = "WARN: require_certificate is false - Mutual authentication is not set and Server will not verifiy the Client's certificate";
 	public final static String NoTLS = "WARN: Neither cafile nor capath set - No TLS/SSL is enabled";
 	public final static String NOCRL = "WARN: Certificate Revocation is not checked";
